b1c33dc662d872bce15236fcbd16b9fe78e0fd46
Amanu Haile <ahaile505@gmail.com>
2019-10-08 16:59:52 -0700

    added models and submitted data

--- adoptions/apps.py
+++ adoptions/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class AdaptionsConfig(AppConfig):
-    name = 'adaptions'
+class AdoptionsConfig(AppConfig):
+    name = 'adoptions'
--- adoptions/models.py
+++ adoptions/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 
 
-
 class Pet(models.Model):
-
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-
-    name  = models.CharField(max_length=100)
-    submitter  = models.CharField(max_length=100)
-    species = models.CharField(max_length=100)
-    breed  = models.CharField(max_length=30)
-    description  = models.TextField(max_length=30)
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
-    submition_date = models.DateTimeField()
+    submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
 
-
-
+    def __str__(self):
+        return self.name
